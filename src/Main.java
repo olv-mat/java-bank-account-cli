79e066989714f7e21fa5c3422b48e6084a3bb32c
Matheus Oliveira <contamatheusprofissional@gmail.com>
2025-07-19 08:15:56 -0300

    feat: initializing account data

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("BANK ACCOUNT");
+        System.out.println("Bank Account");
+        String accountOwner = "Matheus Oliveira";
+        String accountType = "Checking Account";
+        double accountBalance = 2000;
+        System.out.println("=".repeat(30));
+        System.out.println("Account Owner: " + accountOwner);
+        System.out.println("Account Type: " + accountType);
+        System.out.println("Balance: USD " + accountBalance);
+        System.out.println("=".repeat(30));
     }
 }
